c6f151f0156ec8eec03bc1eaaa9bd70e26cac6d1
Phatsakorn Rodphol <phatsakornrodphol@Phatsakorns-MacBook-Pro.local>
2022-06-07 15:28:54 +0700

    init my app

---
+++ New1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your score : ");
+var myScore = Console.ReadLine();
+var numScore = int.Parse(myScore);
+
+if (numScore > 100)
+{
+    Console.WriteLine("Your Garde : Invaild");
+}
+else if (numScore >= 80)
+{
+    Console.WriteLine("Your Garde : A");
+}
+else if (numScore >= 70)
+{
+    Console.WriteLine("Your Garde : B");
+}
+else if (numScore >= 60)
+{
+    Console.WriteLine("Your Garde : C");
+}
+else if (numScore >= 50)
+{
+    Console.WriteLine("Your Garde : D");
+}
+else if (numScore < 0)
+{
+    Console.WriteLine("Your Garde : Invaild");
+}
+else
+{
+    Console.WriteLine("Your Garde : F");
+}
+
