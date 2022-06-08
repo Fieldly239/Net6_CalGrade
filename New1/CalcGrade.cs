b1e638768e2036b4128f387786fedd0a3ed81711
Phatsakorn Rodphol (NTL) <CF303957@tidlor.com>
2022-06-08 15:36:23 +0700

    Add Class CalcGrade

---
+++ New1/CalcGrade.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New1
+{
+    public class CalcGrade
+    {
+        private int Score { get; set; }
+
+        //constructure
+        public CalcGrade(int score)
+        {
+            this.Score = score;
+        }
+
+        public string Excute()
+        {
+            var response = "";
+            if (Score > 100)
+            {
+                response = "Invaild";
+            }
+            else if (Score >= 80)
+            {
+                response = "A";
+            }
+            else if (Score >= 70)
+            {
+                response = "B";
+            }
+            else if (Score >= 60)
+            {
+                response = "C";
+            }
+            else if (Score >= 50)
+            {
+                response = "D";
+            }
+            else if (Score < 0)
+            {
+                response = "Invaild";
+            }
+            else
+            {
+                response = "F";
+            }
+            return response;
+        }
+    }
+}
--- New1/Program.cs
+++ New1/Program.cs
@@ -1,34 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using New1;
+
 Console.Write("Enter your score : ");
 var myScore = Console.ReadLine();
 var numScore = int.Parse(myScore);
 
-if (numScore > 100)
-{
-    Console.WriteLine("Your Garde : Invaild");
-}
-else if (numScore >= 80)
-{
-    Console.WriteLine("Your Garde : A");
-}
-else if (numScore >= 70)
-{
-    Console.WriteLine("Your Garde : B");
-}
-else if (numScore >= 60)
-{
-    Console.WriteLine("Your Garde : C");
-}
-else if (numScore >= 50)
-{
-    Console.WriteLine("Your Garde : D");
-}
-else if (numScore < 0)
-{
-    Console.WriteLine("Your Garde : Invaild");
-}
-else
-{
-    Console.WriteLine("Your Garde : F");
-}
+//if (numScore > 100)
+//{
+//    Console.WriteLine("Your Garde : Invaild");
+//}
+//else if (numScore >= 80)
+//{
+//    Console.WriteLine("Your Garde : A");
+//}
+//else if (numScore >= 70)
+//{
+//    Console.WriteLine("Your Garde : B");
+//}
+//else if (numScore >= 60)
+//{
+//    Console.WriteLine("Your Garde : C");
+//}
+//else if (numScore >= 50)
+//{
+//    Console.WriteLine("Your Garde : D");
+//}
+//else if (numScore < 0)
+//{
+//    Console.WriteLine("Your Garde : Invaild");
+//}
+//else
+//{
+//    Console.WriteLine("Your Garde : F");
+//}
+
+var calcGrade = new CalcGrade(numScore);
+var grade = calcGrade.Excute();
+Console.WriteLine($"Your Garde : {grade}");
 
